91ba5ce76694443b070ddd3fdffece9c35d57083
Jonathan Lindberg <jonathan.c.lindberg@gmail.com>
2022-07-04 02:40:12 +0200

    fix

--- events/message.js
+++ events/message.js
@@ -2,10 +2,14 @@ module.exports = (client, message) => {
     if (message.author.bot || message.channel.type === 'dm') return;
     const prefix = client.config.discord.prefix;
     msg = message.content.toLowerCase();
-    if (msg.indexOf(prefix.toLowerCase) !== 0 && msg.indexOf('.alla' !== 0)) return;
+    boolen = true;
+    if (msg.indexOf(prefix.toLowerCase()) !== 0){
+        boolen = false
+     if(msg.indexOf('.alla')!== 0) return;
+    }
 
 
-    const args = message.content.slice(prefix.length).trim().split(/ +/g);
+    const args = message.content.slice(boolen ? prefix.length : '.alla'.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
 
     const cmd = client.commands.get(command) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(command));
